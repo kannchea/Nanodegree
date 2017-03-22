860a31de7a54a5318a8398c8ecb827c45f805cfb
Mannchuoy Yam <Mann@Apples-Macintosh-5.local>
2017-03-22 11:32:17 -0700

    homework4

--- Homework/main.py
+++ Homework/main.py
@@ -18,6 +18,8 @@ import jinja2
 import webapp2
 import string
 import re
+import random
+import hashlib
 
 from google.appengine.ext import db
 
@@ -159,6 +161,111 @@ class Post(db.Model):
         return render_str("post.html", post = self)
         
         
+        
+#####Unit 4 Homework
+
+def make_salt():
+    return ''.join(random.choice(string.lowercase) for i in range (5))
+
+def make_pw_hash(name, pw):
+    salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s, %s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(",")[1]
+    hash_pw = hashlib.sha256(name + pw + salt).hexdigest()
+    return hash_pw == h.split(',')[0]
+
+class Signup4(Handler):
+    def get(self):
+        self.render("signup4.html")
+    
+    def post(self):
+        
+        user_name = self.request.get('username')
+        pass_word = self.request.get('password')
+        email_    = self.request.get('email')
+        
+        username = valid_username(user_name)
+        password = valid_password(pass_word)
+        verify = self.request.get('verify')
+        email = valid_email(email_)
+        
+        username_err = ""
+        password_err = ""
+        verify_err   = ""
+        email_err    = ""
+        
+        checker = False
+        
+        if not (username):
+            username_err = "That's not a valid username."
+            checker = True 
+        if not (password):
+            password_err = "That wasn't a valid password."
+            checker = True
+        if (password):
+            if not verify or (verify != pass_word):
+                verify_err   = "Your passwords didn't match."
+                checker = True
+        if (email_):
+            if not (email):
+                email_err    = "That's not a valid email."
+                checker = True
+        
+        if checker:
+            self.render("signup4.html", username=user_name, username_err=username_err, password_err = password_err, verify_err=verify_err, email_err=email_err, email=email_)
+        else:
+            
+            password=make_pw_hash(user_name, pass_word)
+            cookie_val = "name=" + user_name + "; password=" + password + "; Path=/"
+            cookie_val = cookie_val.encode('utf8')
+            self.response.headers.add_header('Set-Cookie', cookie_val)
+            self.redirect("/blog/welcome")
+
+class Welcome4(Handler):
+    def get(self):
+        username = self.request.cookies.get('name')
+        self.render("welcome4.html", username=username)
+
+class Login(Handler):
+    def get(self):
+        self.render("login.html")
+    def post(self):
+        
+        user_name = self.request.get('username')
+        pass_word = self.request.get('password')
+        
+        username = valid_username(user_name)
+        password = valid_password(pass_word)
+        
+        checker = False
+        error = ''
+        
+        if not (username):
+            error = "Invalid login."
+            checker = True 
+        if not (password):
+            error = "Invalid login."
+            checker = True
+        
+        if checker:
+            self.render("login.html", username=username, error = error)
+        else:           
+            password=make_pw_hash(user_name, pass_word)
+            cookie_val = "name=" + user_name + "; password=" + password + "; Path=/"
+            cookie_val = cookie_val.encode('utf8')
+            self.response.headers.add_header('Set-Cookie', cookie_val)
+            self.redirect("/blog/welcome")
+            
+
+class Logout(Handler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', "name=; password=; Path=/")
+        self.redirect('/blog/signup')
+
+            
 app = webapp2.WSGIApplication([
     ('/', MainPage), 
     ('/rot13', Rot13), 
@@ -166,5 +273,9 @@ app = webapp2.WSGIApplication([
     ('/welcome', Welcome),
     ('/blog/?', Blog),
     ('/blog/newpost', NewPost),
-    ('/blog/([0-9]+)', PostHandler)
+    ('/blog/([0-9]+)', PostHandler),
+    ('/blog/signup', Signup4),
+    ('/blog/welcome', Welcome4),
+    ('/blog/login', Login),
+    ('/blog/logout', Logout)
 ], debug=True)
